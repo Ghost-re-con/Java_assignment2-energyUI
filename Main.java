38be600049a9cf6dc82b693baef956c2c7040409
Esse Prosperity <132103317+TheAllFather@users.noreply.github.com>
2023-05-12 12:04:54 +0100

    java_second_assignment

---
+++ Main.java
@@ -0,0 +1,11 @@
+package java_second_assignment_GUI;
+
+
+public class Main{
+    public static void main(String[] args) {
+        new EnergyUI();
+    }
+}
+
+
+
---
+++ Physics.java
@@ -0,0 +1,5 @@
+package java_second_assignment_GUI;
+
+public class Physics {
+    public float gravity = 9.8f;
+}
