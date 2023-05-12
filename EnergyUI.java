bdfdc21c06b9951f3d3de7df6cb71a2b124ca429
Esosa013 <132652896+Esosa013@users.noreply.github.com>
2023-05-12 04:45:29 -0700

    Update EnergyUI.java

--- EnergyUI.java
+++ EnergyUI.java
@@ -58,4 +58,77 @@ public class EnergyUI implements ActionListener {
         Height.addActionListener(this);
 
     }
+         public void actionPerformed (ActionEvent e){
+
+        if (e.getSource() == Period) {
+            periodFrame.setVisible(true);
+            periodFrame.setLayout(new GridLayout(7, 1));
+            periodFrame.setSize(150, 200);
+            periodFrame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+            periodFrame.add(length);
+            periodFrame.add(calculatePeriod);
+            calculatePeriod.addActionListener(this);
+
+        } else if (e.getSource() == potentialEnergy) {
+            potentialEnergyFrame.setVisible(true);
+            potentialEnergyFrame.setLayout(new GridLayout(7, 1));
+            potentialEnergyFrame.setSize(150, 200);
+            potentialEnergyFrame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+            potentialEnergyFrame.add(mass);
+            potentialEnergyFrame.add(height);
+            potentialEnergyFrame.add(calculatePotentialEnergy);
+            calculatePotentialEnergy.addActionListener(this);
+
+        } else if (e.getSource() == Force) {
+            forceFrame.setVisible(true);
+            forceFrame.setLayout(new GridLayout(7, 1));
+            forceFrame.setSize(150, 200);
+            forceFrame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+            forceFrame.add(mas);
+            forceFrame.add(calculateForce);
+            calculateForce.addActionListener(this);
+
+        } else if (e.getSource() == Work) {
+            workFrame.setVisible(true);
+            workFrame.setLayout(new GridLayout(7, 1));
+            workFrame.setSize(150, 200);
+            workFrame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+            workFrame.add(force);
+            workFrame.add(displacement);
+            workFrame.add(calculateWork);
+            calculateWork.addActionListener(this);
+
+        } else if (e.getSource() == Height) {
+            heightFrame.setVisible(true);
+            heightFrame.setLayout(new GridLayout(7, 1));
+            heightFrame.setSize(150, 200);
+            heightFrame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+            heightFrame.add(time);
+            heightFrame.add(calculateHeight);
+            calculateHeight.addActionListener(this);
+
+        } else if (e.getSource() == calculatePeriod) {
+            try{JOptionPane.showMessageDialog(null, energy.Period(Float.parseFloat(length.getText())));}
+            catch(Exception f){JOptionPane.showMessageDialog(null,"Use only numbers","Error",JOptionPane.ERROR_MESSAGE);}
+
+        } else if (e.getSource() == calculatePotentialEnergy) {
+            try{JOptionPane.showMessageDialog(null,energy.potentialEnergy(Float.parseFloat(mass.getText()), Float.parseFloat(height.getText())));}
+            catch(Exception f){JOptionPane.showMessageDialog(null,"Use only numbers","Error",JOptionPane.ERROR_MESSAGE);}
+
+        } else if (e.getSource() == calculateForce) {
+            try{JOptionPane.showMessageDialog(null, energy.Force(Float.parseFloat(mas.getText())));}
+            catch(Exception f){JOptionPane.showMessageDialog(null,"Use only numbers","Error",JOptionPane.ERROR_MESSAGE);}
+
+        } else if (e.getSource() == calculateWork) {
+            try{JOptionPane.showMessageDialog(null, energy.Work(Float.parseFloat(force.getText()), Float.parseFloat(displacement.getText())));}
+            catch(Exception f){JOptionPane.showMessageDialog(null,"Use only numbers","Error",JOptionPane.ERROR_MESSAGE);}
+
+        } else if (e.getSource() == calculateHeight) {
+            try{JOptionPane.showMessageDialog(null, energy.Height(Float.parseFloat(time.getText())));}
+            catch(Exception f){JOptionPane.showMessageDialog(null,"Use only numbers","Error",JOptionPane.ERROR_MESSAGE);}
+        }
+    
+
+}
+
 }
